cd7aa37742c858a43d94cebf39c5f90b3f4e6a8a
idobe2 <idobenoun@gmail.com>
2023-03-22 14:00:28 +0200

    tests and eslint added

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ app.get('/example/class',
     (req, res) => {
         console.log("New request arrived");
         res.send('<h1>Hi from class</h1>');
-    })
+    });
 
 app.get('/example',
     (req, res) => {
-        console.log("New request arrived");
+        console.log('New request arrived');
         res.sendFile(path.join(__dirname, '/index.html'));
         // res.send('<h1><Hi from server</h1>');
-    })
+    });
 app.listen(port, ()=>{
-    console.log("Server is running at port " + port)
-})
+    console.log(`Server is running at ${port}`);
+});
