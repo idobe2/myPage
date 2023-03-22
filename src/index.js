16e3813d066f53003cf5cec093caf08402c52d8d
idobe2 <idobe2@ac.sce.ac.il>
2023-03-22 21:52:20 +0200

    HTML plugin installed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import path from 'path';
+
 const port = process.env.port || 3000;
 const app = express();
 
 app.get('/example/class', (req, res) => {
-    console.log('New request arrived');
+    console.log('New request arrived from class');
     res.send('<h1>Hi from class</h1>');
 });
 
