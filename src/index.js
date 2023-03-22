f56d8a06c414516df6e9e43f6f2b925c7f828814
idobe2 <idobenoun@gmail.com>
2023-03-22 13:20:16 +0200

    myPage init

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ const path = require("path");
 const port = process.env.port || 3000;
 const app = express();
 
+app.get('/example/class',
+    (req, res) => {
+        console.log("New request arrived");
+        res.send('<h1>Hi from class</h1>');
+    })
+
 app.get('/example',
     (req, res) => {
-        console.log("new request arrived");
+        console.log("New request arrived");
         res.sendFile(path.join(__dirname, '/index.html'));
+        // res.send('<h1><Hi from server</h1>');
     })
 app.listen(port, ()=>{
-    console.log("server is running at port")
+    console.log("Server is running at port " + port)
 })
