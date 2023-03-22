2b8ad28c17ef8ca88563a10dfaa990e757a498ea
idobe2 <idobe2@ac.sce.ac.il>
2023-03-22 20:24:08 +0200

    code fixing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-import express from "express";
+import express from 'express';
 import path from 'path';
 // const express = require('express');
 // const path = require("path");
 const port = process.env.port || 3000;
 const app = express();
 
+app.get('/example/class', (req, res) => {
+    console.log('New request arrived');
+    res.send('<h1>Hi from class</h1>');
+});
 
-app.get('/example/class',
-    (req, res) => {
-        console.log("New request arrived");
-        res.send('<h1>Hi from class</h1>');
-    });
+app.get('/example', (req, res) => {
+    console.log('New request arrived');
+    res.sendFile(path.join(__dirname, '/index.html'));
+    // res.send('<h1><Hi from server</h1>');
+});
 
-app.get('/example',
-    (req, res) => {
-        console.log('New request arrived');
-        res.sendFile(path.join(__dirname, '/index.html'));
-        // res.send('<h1><Hi from server</h1>');
-    });
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is running at ${port}`);
 });
