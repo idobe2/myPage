b50dd0f5518a69439f39d8ebdbee061e83d255d8
Omer Halfon <omerhalfon1234@gmail.com>
2023-03-23 15:53:34 +0200

    login page

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+
 const port = process.env.port || 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
 
 app.get('/', (req, res) => {
     console.log('New request arrived');
     res.sendFile(path.resolve('index.html'));
 });
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    console.log(`Login request received: username=${username}, password=${password}`);
-    res.send('Login successful!');
+app.get('/Login', (req, res) => {
+    console.log('Redirecting to login page');
+    res.sendFile(path.resolve('login.html'));
 });
 
 
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
 });
-
