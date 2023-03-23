c51d255ab71dac298b7d1ff1f0e6773d1cca843d
Omer Halfon <omerhalfon1234@gmail.com>
2023-03-23 13:52:09 +0200

    ignore

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import path from 'path';
 const port = process.env.port || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('New request arrived from class');
-    res.send("<h1>Hi from class</h1>");
-});
+// app.get('/', (req, res) => {
+//     console.log('New request arrived from class');
+//     res.send("<h1>Hi from class</h1>");
+// });
 
 app.get('/', (req, res) => {
     console.log('New request arrived');
-    res.sendFile(path.resolve('index.html'));
+    res.sendFile(path.resolve(__dirname, 'src', 'index.html'));
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
