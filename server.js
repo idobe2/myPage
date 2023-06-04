b880b742ceeba3a1e8f7bcce9767782137a5b666
Ido <idobenoun@gmail.com>
2023-06-04 16:06:23 +0300

    Revert "set connection method"
    This reverts commit 562e74647bb1ed74da09c1135f92912e777292ae.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
@@ -18,18 +17,4 @@ app.get('/myPage.css', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'myPage.css'));
 });
 
-let isConnected = false; // Flag to track connection status
-
-async function connect() {
-    try {
-        const uri = 'mongodb+srv://mongo:eMyPCnryQVvGKtit@cluster0.oqnshnt.mongodb.net/Grades?retryWrites=true&w=majority';
-        await mongoose.connect(uri);
-        console.log('Connected to URI');
-        isConnected = true; // Update the flag when the connection is successful
-    } catch (error) {
-        console.error('Error connecting to URI: ', error);
-        isConnected = false; // Update the flag when the connection fails
-    }
-}
-
-module.exports = { app, connect, isConnected };
+module.exports = app;
