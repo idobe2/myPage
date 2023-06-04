562e74647bb1ed74da09c1135f92912e777292ae
idobe2 <idobenoun@gmail.com>
2023-06-04 15:13:37 +0300

    set connection method

--- index.js
+++ index.js
@@ -1,62 +1,56 @@
 const mongoose = require('mongoose');
-const app = require('./server');
+const { app, connect } = require('./server');
 
-const uri = 'mongodb+srv://mongo:eMyPCnryQVvGKtit@cluster0.oqnshnt.mongodb.net/Grades?retryWrites=true&w=majority';
+async function startServer() {
+    await connect();
 
-async function connect() {
-    try {
-        await mongoose.connect(uri);
-        console.log('Connected to URI');
-    } catch (error) {
-        console.error('Error connecting to URI: ', error);
-    }
-}
-
-connect();
-
-const gradeSchema = new mongoose.Schema({
-    fullName: String,
-    grade1: Number,
-    grade2: Number,
-    grade3: Number,
-});
-
-const Grades = mongoose.model('Students', gradeSchema); // Use 'Students' as the model name
-
-app.post('/submit-form', async (req, res) => {
-    const {
-        fullName, grade1, grade2, grade3,
-    } = req.body;
-
-    const student = new Grades({
-        fullName,
-        grade1,
-        grade2,
-        grade3,
+    const gradeSchema = new mongoose.Schema({
+        fullName: String,
+        grade1: Number,
+        grade2: Number,
+        grade3: Number,
     });
 
-    try {
-        await student.save(); // Save the student instance
-        console.log('Grades saved successfully');
-        res.redirect('/'); // Redirect back to the myPage.html
-    } catch (err) {
-        console.error('Error saving grades to database:', err);
-        res.sendStatus(500);
-    }
-});
+    const Grades = mongoose.model('Students', gradeSchema); // Use 'Students' as the model name
 
-app.get('/get-grades', async (req, res) => {
-    try {
-        const grades = await Grades.find({}, { _id: 0, __v: 0 }).exec();
-        res.json(grades);
-    } catch (err) {
-        console.error('Error retrieving grades:', err);
-        res.sendStatus(500);
-    }
-});
+    app.post('/submit-form', async (req, res) => {
+        const {
+            fullName, grade1, grade2, grade3,
+        } = req.body;
 
-const port = process.env.PORT || 3000;
+        const student = new Grades({
+            fullName,
+            grade1,
+            grade2,
+            grade3,
+        });
 
-app.listen(port, () => {
-    console.log(`Server is running at ${port}`);
-});
+        try {
+            await student.save(); // Save the student instance
+            console.log('Grades saved successfully');
+            res.redirect('/'); // Redirect back to the myPage.html
+        } catch (err) {
+            console.error('Error saving grades to database:', err);
+            res.sendStatus(500);
+        }
+    });
+
+    app.get('/get-grades', async (req, res) => {
+        try {
+            const grades = await Grades.find({}, { _id: 0, __v: 0 }).exec();
+            res.json(grades);
+        } catch (err) {
+            console.error('Error retrieving grades:', err);
+            res.sendStatus(500);
+        }
+    });
+
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`Server is running at ${port}`);
+    });
+}
+
+startServer();
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
@@ -17,4 +18,18 @@ app.get('/myPage.css', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'myPage.css'));
 });
 
-module.exports = app;
+let isConnected = false; // Flag to track connection status
+
+async function connect() {
+    try {
+        const uri = 'mongodb+srv://mongo:eMyPCnryQVvGKtit@cluster0.oqnshnt.mongodb.net/Grades?retryWrites=true&w=majority';
+        await mongoose.connect(uri);
+        console.log('Connected to URI');
+        isConnected = true; // Update the flag when the connection is successful
+    } catch (error) {
+        console.error('Error connecting to URI: ', error);
+        isConnected = false; // Update the flag when the connection fails
+    }
+}
+
+module.exports = { app, connect, isConnected };
